1008ba8ad219a9ccf9a64f5f75bf66383d70550d
kentaro <kentaro@kentaronoMacBook-Air.local>
2020-05-01 00:09:36 +0900

    ruby課題８、９回答

--- task.rb
+++ task.rb
@@ -65,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,7 +76,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each_with_index do |names, i|
+    p "会員No.#{i+1} #{names}さん"
+    end
 end
 
 def q10
